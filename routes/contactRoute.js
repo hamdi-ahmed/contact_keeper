c8984f2565ea1ff5fd73d985e70434c0df7c07f7
hamdi-ahmed <hamdia108@gmail.com>
2021-10-20 01:57:47 +0200

    Create user contact CRUD operations that allow user to add, remove or update his contacts

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -24,4 +24,79 @@ router.get('/', auth, async (req, res) => {
 	}
 })
 
+// @route     POST api/contact
+// @desc      Add new User Contacts
+// @access    Private
+router.post(
+	'/',
+	[auth, [check('name', 'Name is required').not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req)
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() })
+		}
+		const { name, email, phone, type } = req.body
+		try {
+			const newContact = new Contact({
+				name,
+				email,
+				phone,
+				type,
+				user: req.user.id,
+			})
+			const contact = await newContact.save()
+			res.json(contact)
+		} catch (err) {
+			console.log(`Error: ${err.message}`)
+			res.status(400).send('Server Error')
+		}
+	}
+)
+
+// @route     PUT api/contact/:id
+// @desc      Update User Contacts
+// @access    Private
+router.put('/:id', auth, async (req, res) => {
+	const { name, email, phone, type } = req.body
+	const updatedContact = {}
+	if (name) updatedContact.name = name
+	if (email) updatedContact.email = email
+	if (phone) updatedContact.phone = phone
+	if (type) updatedContact.type = type
+	try {
+		let contact = await Contact.findById(req.params.id)
+		if (!contact) return res.status(404).json({ msg: 'Contact is not found' })
+		if (contact.user.toString() !== req.user.id) {
+			return res.status(404).json({ msg: 'Not Authorized' })
+		}
+		contact = await Contact.findByIdAndUpdate(
+			req.params.id,
+			{ $set: updatedContact },
+			{ new: true }
+		)
+		res.json(contact)
+	} catch (err) {
+		console.log(`Error: ${err.message}`)
+		res.status(400).send('Server Error')
+	}
+})
+
+// @route     DELETE api/contact/:id
+// @desc      delete User Contacts by id
+// @access    Private
+router.delete('/:id', auth, async (req, res) => {
+	try {
+		let contact = await Contact.findByIdAndDelete(req.params.id)
+		if (!contact) return res.status(404).json({ msg: 'Contact is not found' })
+		if (contact.user.toString() !== req.user.id) {
+			return res.status(404).json({ msg: 'Not Authorized' })
+		}
+		await Contact.findByIdAndRemove(req.params.id)
+		res.json({ msg: 'Contact is deleted' })
+	} catch (err) {
+		console.log(`Error: ${err.message}`)
+		res.status(400).send('Server Error')
+	}
+})
+
 module.exports = router
