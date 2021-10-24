06c6925088f2a9bc25b3feea0f671bbc786467bb
hamdi-ahmed <hamdia108@gmail.com>
2021-10-25 01:56:39 +0200

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Modules
 const express = require('express')
 const dbConnect = require('./config/db')
+const path = require('path')
 
 require('dotenv').config()
 const app = express()
@@ -15,6 +16,16 @@ app.use(express.json({ extended: false }))
 app.use('/api/auth', require('./routes/authRoute'))
 app.use('/api/user', require('./routes/userRoute'))
 app.use('/api/contact', require('./routes/contactRoute'))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'))
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	)
+}
 
 // Setup the port and Run the server
 const port = process.env.PORT || 8000
